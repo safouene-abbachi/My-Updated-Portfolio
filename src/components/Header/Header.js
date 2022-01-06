57e2f425daeb6ceccd799de150e500f24c035735
safouene <safouene-abbachi@hotmail.fr>
2022-01-06 17:00:11 +0100

    DEV : Finishing header and Hero part

---
+++ src/components/Header/Header.js
@@ -0,0 +1,64 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillFacebook, AiFillLinkedin } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di';
+
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  Span,
+  NavLink,
+  SocialIcons,
+} from './HeaderStyles';
+
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            color: 'white',
+            marginBottom: '20px',
+          }}
+        >
+          <DiCssdeck size="3rem" />
+          <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#teck">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/safouene-abbachi">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/safouene-abbachi/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.facebook.com/safouene.abbachi">
+        <AiFillFacebook size="3rem" />
+      </SocialIcons>
+    </Div3>
+  </Container>
+);
+
+export default Header;
